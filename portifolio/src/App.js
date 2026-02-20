a1535bdec92ae68c0c06ec5ed58e9cb91f82e632
Jvictorifran <joaovictorcandidoifran@gmail.com>
2026-02-20 15:54:14 -0400

    salvand atts

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //pages
 import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- portifolio/src/components/pages/Home.jsx
+++ portifolio/src/components/pages/Home.jsx
@@ -3,15 +3,22 @@ import Footer from '../layout/Footer';
 import Projetos from '../layout/Projetos';
 
 import styles from './Home.module.css'
+import img from '../../img/jv.png'
 
 function Home(){
     return(
         <main>
-            <h1>Home</h1>
-            <Projetos/>
-            <Projetos/>
-            <Projetos/>
-            <Footer/>   
+            <div>
+                <h1>João Ifran</h1>
+                <h2>Hello, it's me </h2>
+                <p>
+                    Transformar problemas complexos em código limpo é o que me move. Como futuro Engenheiro de Computação,  combino a análise técnica profunda com a agilidade  do desenvolvimento web moderno. 
+                    Minha especialidade é orquestrar dados e regras de  negócio no Back-End utilizando Python (Django) e  PHP, mas não paro por aí. Atuo também no Front-End  com React e JavaScript, fechando o ciclo do  desenvolvimento Full Stack. Meu objetivo é entregar  aplicações completas: seguras no back-end e intuitivas no front-end.
+                </p>
+            </div>
+            <div>
+                <img src={img} alt="" />
+            </div>
         </main>
     )
     
